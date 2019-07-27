051cb54e5a6754c8ed39fac7deedee8f7b181fd6
Oscar cantillo <oscantillomen@gmail.com>
2019-07-27 12:37:25 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Title from './components/Title';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div>
+        <div>
+          <Title></Title>
+          <h2>{count}</h2>
+          <button onClick={() => setCount(count + 1)}>Crear cuenta gratuita</button>
+          <img src={process.env.PUBLIC_URL + '/images/train.jpg'} alt="main" height="300" />
+          <div>
+            <ul>
+              <li>
+                <h3>Calificaciones con emociones</h3>
+                <p>Calificac tus lugares con experiencias, no con números</p>
+              </li>
+              <li>
+                <h3>¿Sin Internet? Sin problemas</h3>
+                <p>Places funciona si internet o en conexiones lentas</p>
+              </li>
+              <li>
+                <h3>Tus lugares favoritos</h3>
+                <p>Define tu lista de sitios favoritos</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
