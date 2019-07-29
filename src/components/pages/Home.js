29e5a7f6ca64dc2c8f87461b590bbd1a80ce1661
oscantillomen <oscantillomen@gmail.com>
2019-07-28 21:56:10 -0500

    Working in transitions

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 import RaisedButton from  'material-ui/RaisedButton';
-
 import { indigo400 } from 'material-ui/styles/colors';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
 
-import Title from '../Title';
 import PlaceCard from '../../components/places/PlaceCard'
-import data from '../../request/places';
+import Title from '../Title';
+
 import Benefits from '../Benefits';
+
+import data from '../../request/places';
 
 export default class Home extends Component {
     constructor(props){
         super(props);    
 
         this.state = {
-            places: data.places,
+            places: []
         }
 
+        setTimeout(() => {
+            this.setState({places: data.places});
+        }, 3000);
         this.hidePlace = this.hidePlace.bind(this);
     }
 
@@ -29,7 +34,7 @@ export default class Home extends Component {
     }
     hidePlace(place){
         this.setState({
-            places: this.state.places.filter(el => el != place)
+            places: this.state.places.filter(el => el !== place)
         });
     }
     render(){
@@ -46,9 +51,9 @@ export default class Home extends Component {
                     </div>
                     <div style={{'backgroundColor': indigo400, 'padding': '50px'}}>
                         <h3 style={{'fontSize': '24px'}}>Sitios Populares</h3>
-                        <div className="row">
+                        <TransitionGroup className="row">
                             {this.places()}
-                        </div>
+                        </TransitionGroup>
                     </div>
                 </div>
             </div>
--- src/components/places/PlaceCard.js
+++ src/components/places/PlaceCard.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
 import { Card, CardText, CardMedia, CardTitle, CardActions } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
+import { FlatButton} from 'material-ui/FlatButton';
+import { CSSTransition } from 'react-transition-group';
 
 export default class PlaceCard extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            show: true
+        }
+    }
     render(){
         return(
-            <div className="col-xs-12 col-md-4" key={this.props.index}>
-                <Card>
-                    <CardMedia>
-                        <img src={process.env.PUBLIC_URL + this.props.place.imageUrl } alt=""/>
-                    </CardMedia>
-                    <CardTitle title={this.props.place.title}></CardTitle>
-                    <CardText>{this.props.place.description}</CardText>
-                    <CardActions>
-                        <FlatButton secondary={true} label='Ver más'></FlatButton>
-                        <FlatButton secondary={true} label='Ocultar' onClick={() => this.props.onRemove(this.props.place)}></FlatButton>
-                    </CardActions>
-                </Card>
-            </div>
+            <CSSTransition classNames='fade-scale' in={this.props.in}>
+                <div className="col-xs-12 col-md-4" key={this.props.index}>
+                    <Card>
+                        <CardMedia>
+                            <img src={process.env.PUBLIC_URL + this.props.place.imageUrl } alt=""/>
+                        </CardMedia>
+                        <CardTitle title={this.props.place.title}></CardTitle>
+                        <CardText>{this.props.place.description}</CardText>
+                        <CardActions>
+                            <FlatButton secondary={true} label='Ver más'></FlatButton>
+                            <FlatButton secondary={true} label='Ocultar' onClick={() => this.props.onRemove(this.props.place)}></FlatButton>
+                        </CardActions>
+                    </Card>
+                </div>
+            </CSSTransition>
         );
     }
 }
